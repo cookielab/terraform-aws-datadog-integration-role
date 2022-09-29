c43386b295423cf29906a9b5b4d9b7c9d96cb90d
Patrik Votoček <patrik@votocek.cz>
2022-09-29 14:48:02 +0200

    add datadog aws account id to outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "role_name" {
   description = "IAM role name"
   value       = aws_iam_role.datadog_integration.name
 }
+
+output "datadog_account_id" {
+  description = "Datadog AWS account id"
+  value       = var.datadog_account_id
+}
